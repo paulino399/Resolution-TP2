9d3160c3a4b08162ffeb3c83e2586382d92ae614
João Paulino <joaopaulino399@gmail.com>
2023-10-21 10:52:44 +0200

    tp35

---
+++ TP3/01/05/exercice_5_TP3.c
@@ -0,0 +1,8 @@
+/////////////////////////////////////////////////////////////////////////////
+//On désire toujours afficher sur les leds d'une carte EB004 (LED board) 
+//connectée à un port du MCU, l'état desinterrupteurs de la carte avec 8 INTER 
+//connectée au circuit PCF8574. MAIS on vous demande de créer une fonction 
+//« input_portF(...) » qui permet de recevoir un octet sur le portcréé par le
+//circuit PCF8574.
+///////////////////////////////////////////////////////////////////////////////
+
